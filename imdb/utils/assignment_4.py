459595753e5a8c2cd47a736356612a618bedb823
dchaitu <chaitanyadokara@gmail.com>
2022-08-19 20:40:07 +0530

    Assignment 4 get_no_of_movies_and_distinct_roles_for_each_actor. Aggregations need to be used

--- imdb/utils/assignment_4.py
+++ imdb/utils/assignment_4.py
@@ -26,7 +26,9 @@ def get_male_and_female_actors_count_for_each_movie():
     :return: a list of Movie model instances
     """
 
-    movies = Movie.objects.values('actors').annotate(male_count=Count('actors',filter=Q(gender='M')),female_count=Count('actors',filter=Q(gender='F')))
+    movies = Movie.objects.annotate(male_count=Count('actors', filter=Q(actors__gender='M')),
+                                    female_count=Count('actors', filter=Q(actors__gender='F'))).values('male_count',
+                                                                                                       'female_count')
     return movies
 
 def get_roles_count_for_each_movie():
@@ -62,8 +64,17 @@ def get_no_of_movies_and_distinct_roles_for_each_actor():
     """
     :return: a list of Movie model instances
     """
-
-    actors = Actor.objects.values('actor_id').annotate(Count('movie__cast__role',distinct=True)).annotate(Count('movie__movie_id'))
+    # Select
+    # Count(*), imdb_actor.name
+    # FROM
+    # imdb_cast, imdb_actor
+    # Where
+    # imdb_cast.actor_id = imdb_actor.actor_id
+    # Group
+    # By
+    # imdb_cast.actor_id
+    # actors = Actor.objects.values('actor_id').annotate(Count('movie__cast__role',distinct=True)).annotate(Count('movie__movie_id'))
+    actors = Cast.objects.values('actor__actor_id').annotate(Count('actor'))
     return actors
 # Check
 
